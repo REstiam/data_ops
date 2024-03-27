1e00784b6b1267b3a68ab8a29ad2258d139d55c8
REstiam <164189454+REstiam@users.noreply.github.com>
2024-03-27 15:40:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,12 +38,13 @@ numerical_column = col_donnees
 fig = px.line(df, x=col_date, y=col_donnees, title="Consommation totale de la semaine")
 st.plotly_chart(fig)
 
-# Calculer la consommation totale de la semaine
-df_weekly_total = calculer_consommation_totale_semaine(df, col_date, col_donnees)
+# Calculer la consommation totale sur la dernière semaine de chaque mois
+df_last_week_monthly = calculer_consommation_totale_semaine(df, "date", "consommation")
 
-st.write("Consommation totale de la semaine:")
-st.write(df_weekly_monthly)
+# Afficher les données dans un tableau
+st.write("Consommation totale sur la dernière semaine de chaque mois :")
+st.write(df_last_week_monthly)
 
 # Créer la charte graphique en cercle
-fig = px.pie(df_last_week_monthly, values='consommation', names='date', title='Consommation totale sur la dernière semaine de chaque mois')
+fig = px.pie(df_last_week_monthly, values='consommation', names='date', title="Consommation totale de la semaine")
 st.plotly_chart(fig, use_container_width=True)
