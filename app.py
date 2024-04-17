30320cb7a86fb9bb80837e2d417910d0808810b3
REstiam <164189454+REstiam@users.noreply.github.com>
2024-04-17 14:14:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from src.fetch_data import load_data_from_lag_to_today
-from src.process_data import col_date, col_donnees, main_process, fic_export_data, calculer_consommation_totale_semaine
+from src.process_data import col_date, col_donnees, main_process, fic_export_data
 import logging
 import os
 import glob
 
+import requests
+from dotenv import load_dotenv
+ 
+load_dotenv()
+
+from schedule import every, repeat
+import schedule
+import time
+
+
 logging.basicConfig(level=logging.INFO)
 
 LAG_N_DAYS: int = 7
+LENGTH_DATA: int = 0
 
 os.makedirs("data/raw/", exist_ok=True)
 os.makedirs("data/interim/", exist_ok=True)
@@ -45,15 +56,16 @@ st.subheader("Line Chart of Numerical Data Over Time")
 
 numerical_column = col_donnees
 
-fig = px.line(df, x=col_date, y=col_donnees, title="Consommation totale de la semaine")
-st.plotly_chart(fig)
+while True:
+    schedule.run_pending()
+    df: pd.DataFrame = pd.read_csv(fic_export_data, parse_dates=[col_date])
+    if len(df) > LENGTH_DATA:
+        LENGTH_DATA = len(df)
+        logging.info(f"Nb points de mesure: {LENGTH_DATA}")
+        st.toast("Nouvelles données disponibles", icon="🎉")
+    requests.post(
+        os.environ["BLOWERIO_URL"] + "/messages",
+        data={"to": "+33783112476", "message": "Test du SMS"},
+    )
 
-# Calculer la consommation totale de la semaine
-df_weekly_total = calculer_consommation_totale_semaine(df, col_date, col_donnees)
-
-# Afficher la consommation totale de la semaine
-st.subheader("Consommation totale de la semaine")
-st.write(df_weekly_total)
-
-# Appel de la fonction pour afficher les données supprimées
-display_removed_data(remove_data)
+    time.sleep(60)
